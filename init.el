d9d0349974d36e2f25f4b7ff9e531fed574bec38
Jaeho Hur <jaeho.hur@sk.com>
2018-04-23 15:53:19 +0900

    neotree projectiles action enabled.

--- init.el
+++ init.el
@@ -244,6 +244,21 @@
 (setq neo-window-width window-min-width)
 ;(setq neo-window-position 'right)
 (setq neo-window-fixed-size nil)
+(setq neo-smart-open t)
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+        (file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+        (if (neo-global--window-exists-p)
+            (progn
+              (neotree-dir project-dir)
+              (neotree-find file-name)))
+      (message "Could not find git project root."))))
 
 ;;==============================================================================
 ;; pdf-tools
