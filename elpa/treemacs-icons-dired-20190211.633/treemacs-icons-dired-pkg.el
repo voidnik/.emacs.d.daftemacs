5b0c56c68b51a2d70f6a64b97f46d6fb6517be72
Jaeho Hur <jaeho.hur@sk.com>
2019-02-13 10:52:13 +0900

    Do not cache 'elpa/treemacs-*'.

--- elpa/treemacs-icons-dired-20190211.633/treemacs-icons-dired-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-icons-dired" "20190211.633" "Treemacs icons for dired" '((treemacs "0.0") (emacs "25.2") (cl-lib "0.5")) :commit "f5f31eee98e0cd573ec46bb2526b20d7bf4a2a1a" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
