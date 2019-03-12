56071089386298efe7f7dbb23f37e2846b9c3d9d
Richard Hur <daftcoder@gmail.com>
2019-03-12 13:58:43 +0900

    doom-modeline added.

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 (message "system-type: %s" system-type)
 (message "system-name: %s" system-name)
 (message "user-login-name: %s" user-login-name)
-(message "user-init-file: %s (last modified date: 2019-02-11)" user-init-file)
+(message "user-init-file: %s (last modified date: 2019-03-12)" user-init-file)
 (message "user-emacs-directory: %s" user-emacs-directory)
 
 (setq gc-cons-threshold (* 100 1024 1024))
@@ -57,15 +57,17 @@
   (interactive)
   (find-file user-init-file))
 
-(defun init-doom-theme-mod ()
-  (add-to-list 'load-path "~/.emacs.d/doom-themes-20180328.1556-mod/")
+(defun init-doom-theme ()
   (require 'doom-themes)
+
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-  ;; may have their own settings.
+
+  ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each
+  ;; theme may have their own settings.
   ;(load-theme 'doom-one t)
+  ;(load-theme 'doom-dracula t)
   ;(load-theme 'doom-city-lights t)
   ;(load-theme 'doom-molokai t)
   ;(load-theme 'doom-nord t)
@@ -73,14 +75,104 @@
   ;(load-theme 'doom-peacock t)
   ;(load-theme 'doom-solarized-light t)
   ;(load-theme 'doom-spacegrey t)
-  (load-theme 'doom-tomorrow-night t)
+  ;(load-theme 'doom-tomorrow-night t)
   ;(load-theme 'doom-tomorrow-day t)
+
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
+
   ;; Enable custom neotree theme
   (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
+
+(defun init-doom-modeline ()
+  (use-package doom-modeline
+    :ensure t
+    :hook (after-init . doom-modeline-mode))
+
+  ;; How tall the mode-line should be (only respected in GUI Emacs).
+  (setq doom-modeline-height 25)
+
+  ;; How wide the mode-line bar should be (only respected in GUI Emacs).
+  (setq doom-modeline-bar-width 3)
+
+  ;; Determines the style used by `doom-modeline-buffer-file-name'.
+  ;;
+  ;; Given ~/Projects/FOSS/emacs/lisp/comint.el
+  ;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
+  ;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
+  ;;   truncate-with-project => emacs/l/comint.el
+  ;;   truncate-except-project => ~/P/F/emacs/l/comint.el
+  ;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
+  ;;   truncate-all => ~/P/F/e/l/comint.el
+  ;;   relative-from-project => emacs/lisp/comint.el
+  ;;   relative-to-project => lisp/comint.el
+  ;;   file-name => comint.el
+  ;;   buffer-name => comint.el<2> (uniquify buffer name)
+  ;;
+  ;; If you are expereicing the laggy issue, especially while editing remote files
+  ;; with tramp, please try `file-name' style.
+  ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
+  (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
+
+  ;; Whether show `all-the-icons' or not (if nil nothing will be showed).
+  (setq doom-modeline-icon t)
+
+  ;; Whether show the icon for major mode. It respects `doom-modeline-icon'.
+  (setq doom-modeline-major-mode-icon t)
+
+  ;; Display color icons for `major-mode'. It respects `all-the-icons-color-icons'.
+  (setq doom-modeline-major-mode-color-icon nil)
+
+  ;; Whether display minor modes or not. Non-nil to display in mode-line.
+  (setq doom-modeline-minor-modes nil)
+
+  ;; If non-nil, a word count will be added to the selection-info modeline segment.
+  (setq doom-modeline-enable-word-count nil)
+
+  ;; If non-nil, only display one number for checker information if applicable.
+  (setq doom-modeline-checker-simple-format t)
+  
+  ;; Whether display perspective name or not. Non-nil to display in mode-line.
+  (setq doom-modeline-persp-name t)
+
+  ;; Whether display `lsp' state or not. Non-nil to display in mode-line.
+  (setq doom-modeline-lsp t)
+
+  ;; Whether display github notifications or not. Requires `ghub` package.
+  (setq doom-modeline-github nil)
+
+  ;; The interval of checking github.
+  (setq doom-modeline-github-interval (* 30 60))
+
+  ;; Whether display environment version or not
+  (setq doom-modeline-env-version t)
+  ;; Or for individual languages
+  (setq doom-modeline-env-enable-python t)
+  (setq doom-modeline-env-enable-ruby t)
+  (setq doom-modeline-env-enable-perl t)
+  (setq doom-modeline-env-enable-go t)
+  (setq doom-modeline-env-enable-elixir t)
+  (setq doom-modeline-env-enable-rust t)
+
+  ;; Change the executables to use for the language version string
+  (setq doom-modeline-env-python-executable "python")
+  (setq doom-modeline-env-ruby-executable "ruby")
+  (setq doom-modeline-env-perl-executable "perl")
+  (setq doom-modeline-env-go-executable "go")
+  (setq doom-modeline-env-elixir-executable "iex")
+  (setq doom-modeline-env-rust-executable "rustc")
+
+  ;; Whether display mu4e notifications or not. Requires `mu4e-alert' package.
+  (setq doom-modeline-mu4e t)
+
+  ;; Whether display irc notifications or not. Requires `circe' package.
+  (setq doom-modeline-irc t)
+
+  ;; Function to stylize the irc buffer names.
+  (setq doom-modeline-irc-stylize 'identity))
 
 (defun init-zerodark-theme ()
   (load-theme 'zerodark t)
@@ -99,10 +191,11 @@
 
 (defun init-themes ()
   ;(load-theme 'base16-default-dark t)
-  ;(init-doom-theme-mod)
+  ;(init-doom-theme)
   ;(init-zerodark-theme)
   ;(init-color-theme)
   (load-theme 'dracula t) ; https://draculatheme.com/emacs/
+  (init-doom-modeline)
   )
 
 ;** Font Repositories **
