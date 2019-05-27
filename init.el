353ec25c8e552df7e43e58ee2f740ea24e9f9473
Jaeho Hur <jaeho.hur@sk.com>
2019-05-27 15:20:39 +0900

    projectile-enable-caching enabled.

--- init.el
+++ init.el
@@ -364,6 +364,7 @@
 ;;==============================================================================
 
 (projectile-mode +1)
+(setq projectile-enable-caching t)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
