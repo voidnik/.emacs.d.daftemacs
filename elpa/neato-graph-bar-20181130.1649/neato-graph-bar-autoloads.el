1ce0ed17e84c02ec2fab35bab08e5979929baf87
Richard Hur <daftcoder@gmail.com>
2019-05-29 14:07:57 +0900

    neato-graph-bar added.

---
+++ elpa/neato-graph-bar-20181130.1649/neato-graph-bar-autoloads.el
@@ -0,0 +1,33 @@
+;;; neato-graph-bar-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "neato-graph-bar" "neato-graph-bar.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from neato-graph-bar.el
+
+(autoload 'neato-graph-bar "neato-graph-bar" "\
+Displays system information graph bars.
+
+\(fn)" t nil)
+
+(autoload 'neato-graph-bar-mode "neato-graph-bar" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "neato-graph-bar" '("neato-graph-bar-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; neato-graph-bar-autoloads.el ends here
--- init.el
+++ init.el
@@ -313,7 +313,7 @@
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet elogcat rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump focus smart-mode-line google-c-style dracula-theme ccls company-lsp lsp-ui lsp-mode flycheck treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs pdf-tools helm-gtags imenu-list objc-font-lock neotree zerodark-theme company magit vlf base16-theme flx-isearch flx-ido flx projectile dark-souls haskell-mode))))
+    (neato-graph-bar importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet elogcat rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump focus smart-mode-line google-c-style dracula-theme ccls company-lsp lsp-ui lsp-mode flycheck treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs pdf-tools helm-gtags imenu-list objc-font-lock neotree zerodark-theme company magit vlf base16-theme flx-isearch flx-ido flx projectile dark-souls haskell-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -972,6 +972,7 @@
 (global-set-key (kbd "C-c 1") 'eshell)
 ;(global-set-key (kbd "C-c 1") 'shell)
 ;(global-set-key (kbd "C-c 1") 'term)
+(global-set-key (kbd "C-c 2") 'neato-graph-bar)
 (global-set-key (kbd "C-c _") 'whitespace-mode)
 (global-set-key (kbd "C-c =") 'hexl-mode-toggle)
 
