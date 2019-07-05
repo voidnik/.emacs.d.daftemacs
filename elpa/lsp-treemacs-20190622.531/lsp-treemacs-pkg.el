96ca82c816c3e0c4046cf48a828affd7787dca1f
Jaeho Hur <jaeho.hur@sk.com>
2019-07-05 20:28:26 +0900

    packages upgraded.

--- elpa/lsp-treemacs-20190622.531/lsp-treemacs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-treemacs" "20190622.531" "LSP treemacs" '((emacs "25.1") (dash "2.14.1") (dash-functional "2.14.1") (f "0.20.0") (ht "2.0") (treemacs "2.5") (lsp-mode "6.0")) :commit "258d97609079983c55339b29994254aaf7eaeab8" :keywords '("languages") :authors '(("Ivan Yonchovski")) :maintainer '("Ivan Yonchovski"))
---
+++ elpa/lsp-treemacs-20190627.1312/lsp-treemacs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-treemacs" "20190627.1312" "LSP treemacs" '((emacs "25.1") (dash "2.14.1") (dash-functional "2.14.1") (f "0.20.0") (ht "2.0") (treemacs "2.5") (lsp-mode "6.0")) :commit "f652cae484cb9c172c832447f7289aa00380159b" :keywords '("languages") :authors '(("Ivan Yonchovski")) :maintainer '("Ivan Yonchovski"))
