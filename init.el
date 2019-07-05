c4ccb3e244bc89c73508d3a3df3027ec44d64551
Jaeho Hur <jaeho.hur@sk.com>
2019-07-05 20:07:26 +0900

    highlight-indent-guides installed.

--- init.el
+++ init.el
@@ -320,7 +320,7 @@
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (lsp-treemacs treemacs-icons-dired counsel magit-gitflow keyfreq neato-graph-bar importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet elogcat rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump focus smart-mode-line google-c-style dracula-theme ccls company-lsp lsp-ui lsp-mode flycheck treemacs-magit treemacs-projectile treemacs-evil treemacs pdf-tools helm-gtags imenu-list objc-font-lock neotree zerodark-theme company magit vlf base16-theme flx-isearch flx-ido flx projectile dark-souls haskell-mode))))
+    (highlight-indent-guides lsp-treemacs treemacs-icons-dired counsel magit-gitflow keyfreq neato-graph-bar importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet elogcat rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump focus smart-mode-line google-c-style dracula-theme ccls company-lsp lsp-ui lsp-mode flycheck treemacs-magit treemacs-projectile treemacs-evil treemacs pdf-tools helm-gtags imenu-list objc-font-lock neotree zerodark-theme company magit vlf base16-theme flx-isearch flx-ido flx projectile dark-souls haskell-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -817,6 +817,16 @@
 (add-hook 'js-mode-hook
           (lambda()
             (setq js-indent-level 2)))
+
+;;==============================================================================
+;; highlight-indent-guides
+;;
+;; https://github.com/DarthFennec/highlight-indent-guides
+;;==============================================================================
+
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+(setq highlight-indent-guides-method 'character)
+(setq highlight-indent-guides-responsive 'top)
 
 ;;;==============================================================================
 ;;; Dim for #if 0 ... #endif
