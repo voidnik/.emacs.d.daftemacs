30c47a016bd59090b7c689a65906255ac4a5f1f7
Jaeho Hur <jaeho.hur@sk.com>
2019-10-22 15:15:18 +0900

    packages upgraded.

--- elpa/swiper-20191016.1657/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/daftcoder/.emacs.d/elpa/swiper-20191016.1657/swiper.el  -*- no-byte-compile: t -*-
-(define-package "swiper" "20191016.1657" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "be231a50c6e9561df1bde4e5c68ca199e9a92c49" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
