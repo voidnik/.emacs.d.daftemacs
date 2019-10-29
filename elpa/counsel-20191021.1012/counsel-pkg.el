f650565fe1d3d5fd56d72b7f6d04b63da5225540
Jaeho Hur <jaeho.hur@sk.com>
2019-10-29 18:05:17 +0900

    packages upgraded.

--- elpa/counsel-20191021.1012/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20191021.1012" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "a2a3263e3df9a2c3e24de442f9bf108d023e0978" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
