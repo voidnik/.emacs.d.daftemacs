b19c13424c52ccecd89c30a8abb4b08d5b0fe201
Richard Jaeho Hur <daftcoder@gmail.com>
2020-05-27 00:43:48 +0900

    Some elpa packages removed from git.

--- elpa/qml-mode-20161016.31/qml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/daftcoder/.emacs.d/elpa/qml-mode-20161016.31/qml-mode.el  -*- no-byte-compile: t -*-
-(define-package "qml-mode" "20161016.31" "Major mode for editing QT Declarative (QML) code." 'nil :commit "6c5f33ba88ae010bf201a80ee8095e20a724558c" :keywords '("qml" "qt" "qt declarative") :authors '(("Yen-Chin Lee" . "coldnew.tw@gmail.com")) :maintainer '("Yen-Chin Lee" . "coldnew.tw@gmail.com") :url "https://github.com/coldnew/qml-mode")
--- elpa/vlf-20191126.2250/vlf-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "vlf" "20191126.2250" "View Large Files" 'nil :keywords
-  '("large files" "utilities")
-  :maintainer
-  '("Andrey Kotlarski" . "m00naticus@gmail.com")
-  :url "https://github.com/m00natic/vlfi")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
