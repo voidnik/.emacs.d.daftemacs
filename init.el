9e96ed3e3929148e6866a87c83752cf81dc9591b
Jaeho Hur <jaeho.hur@sk.com>
2020-12-01 03:00:01 +0900

    'M-SPC' is mapped to 'toggle-input-method'.

--- init.el
+++ init.el
@@ -1601,6 +1601,7 @@
 ;;==============================================================================
 
 (global-set-key (kbd "S-SPC") 'toggle-input-method)
+(global-set-key (kbd "M-SPC") 'toggle-input-method)
 
 (global-set-key (kbd "M-o") 'ace-window)
 (global-set-key (kbd "M-m") 'imenu-list)
