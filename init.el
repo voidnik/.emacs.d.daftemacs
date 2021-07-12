3f8136717cda396863fc25dec543cdd25c6d2376
Richard Jaeho Hur <daftcoder@gmail.com>
2021-07-13 02:29:46 +0900

    importmagic-python-interpreter added.

--- init.el
+++ init.el
@@ -1194,7 +1194,8 @@
 (use-package importmagic
   :ensure t
   :config
-  (add-hook 'python-mode-hook 'importmagic-mode))
+  (add-hook 'python-mode-hook 'importmagic-mode)
+  (setq importmagic-python-interpreter "~/anaconda3/bin/python3"))
 
 (use-package pyvenv
   :ensure t
