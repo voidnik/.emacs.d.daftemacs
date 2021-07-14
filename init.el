da29b7417ca7d3b4a58fd862ce7145831b002ed8
Richard Jaeho Hur <daftcoder@gmail.com>
2021-07-14 14:35:43 +0900

    dashboard treats page-break-lines as the optional package.

--- init.el
+++ init.el
@@ -86,7 +86,7 @@
      ("\\.x?html?\\'" . default)
      ("\\.pdf\\'" . emacs)))
  '(package-selected-packages
-   '(exec-path-from-shell yaml-mode hide-mode-line lsp-pyright centaur-tabs use-package bind-key fcitx dashboard google-c-style i3wm-config-mode peep-dired swift-mode focus cuda-mode org-bullets bufler org-re-reveal markdown-preview-mode graphviz-dot-mode ivy counsel counsel-projectile swiper ivy-posframe ivy-rich all-the-icons-ivy all-the-icons-ivy-rich lsp-ivy diff-hl company-statistics treemacs-icons-dired qml-mode highlight-indent-guides lsp-treemacs keyfreq neato-graph-bar epc importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump ccls lsp-ui lsp-mode flycheck spell-fu treemacs-magit treemacs-projectile treemacs-evil treemacs pdf-tools helm-gtags imenu-list objc-font-lock neotree company magit vlf flx-isearch flx-ido flx projectile haskell-mode lua-mode ztree undo-tree shrink-path rich-minority pyvenv markdown-mode magit-popup highlight-indentation helm find-file-in-project evil doom-themes doom-modeline avy all-the-icons ace-window)))
+   '(page-break-lines exec-path-from-shell yaml-mode hide-mode-line lsp-pyright centaur-tabs use-package bind-key fcitx dashboard google-c-style i3wm-config-mode peep-dired swift-mode focus cuda-mode org-bullets bufler org-re-reveal markdown-preview-mode graphviz-dot-mode ivy counsel counsel-projectile swiper ivy-posframe ivy-rich all-the-icons-ivy all-the-icons-ivy-rich lsp-ivy diff-hl company-statistics treemacs-icons-dired qml-mode highlight-indent-guides lsp-treemacs keyfreq neato-graph-bar epc importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump ccls lsp-ui lsp-mode flycheck spell-fu treemacs-magit treemacs-projectile treemacs-evil treemacs pdf-tools helm-gtags imenu-list objc-font-lock neotree company magit vlf flx-isearch flx-ido flx projectile haskell-mode lua-mode ztree undo-tree shrink-path rich-minority pyvenv markdown-mode magit-popup highlight-indentation helm find-file-in-project evil doom-themes doom-modeline avy all-the-icons ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -345,6 +345,15 @@
     (setup-font))
 
 ;;==============================================================================
+;; page-break-lines
+;;
+;; https://github.com/purcell/page-break-lines
+;;==============================================================================
+
+(use-package page-break-lines
+  :ensure t)
+
+;;==============================================================================
 ;; dashboard
 ;;
 ;; https://github.com/emacs-dashboard/emacs-dashboard
@@ -361,7 +370,9 @@
                           (bookmarks . 10)
                           (projects . 10)
                           (agenda . 10)
-                          (registers . 10))))
+                          (registers . 10)))
+  (setq dashboard-page-separator "\n\f\n") ;; This depends on page-break-lines.
+  )
 
 ;;==============================================================================
 ;; centaur-tabs
