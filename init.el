03cf7623e6ceb394853285cb5afee64a65d0dec0
Richard Jaeho Hur <daftcoder@gmail.com>
2021-07-19 23:52:14 +0900

    Added 'ein:preview-md-cell-latex' for LaTeX preview in EIN.

--- init.el
+++ init.el
@@ -450,6 +450,7 @@
        (string-prefix-p " *temp" name)
        (string-prefix-p "*Help" name)
        (string-prefix-p "*Bufler" name)
+       (string-prefix-p "*ein: LaTeX in Markdown preview*" name)
 
        ;; Is not magit buffer.
        (and (string-prefix-p "magit" name)
@@ -1253,7 +1254,33 @@
 ;; EIN -- Emacs IPython Notebook
 ;; https://github.com/millejoh/emacs-ipython-notebook
 (use-package ein
-  :ensure t)
+  :ensure t
+  :config
+  ;; https://github.com/millejoh/emacs-ipython-notebook/issues/88
+  (defun ein:preview-md-cell-latex ()
+    "Preview LaTeX from the current markdown cell in a separate buffer."
+    (interactive)
+    (let* ((cell (ein:worksheet-get-current-cell))
+	       (buffer (if (ein:markdowncell-p cell)
+		               (get-buffer-create "*ein: LaTeX in Markdown preview*")
+		             (error "Not on a markdown cell"))))
+      (with-current-buffer buffer
+        (when buffer-read-only
+	      (toggle-read-only))
+        (unless (= (point-min) (point-max))
+	      (delete-region (point-min) (point-max)))
+        (insert (slot-value cell :input))
+        (goto-char (point-min))
+        (org-mode)
+        (org-toggle-latex-fragment 16)
+        (special-mode)
+        (unless buffer-read-only
+	      (toggle-read-only))
+        (display-buffer
+         buffer
+         '((display-buffer-below-selected display-buffer-at-bottom)
+           (inhibit-same-window . t)))
+        (fit-window-to-buffer (window-in-direction 'below))))))
 
 ;;==============================================================================
 ;; haskell-mode
