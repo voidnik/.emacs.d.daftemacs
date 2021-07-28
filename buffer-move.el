b8c157a45e35cb9e5a893b9bd387434f6f71f55d
Richard Jaeho Hur <daftcoder@gmail.com>
2021-07-29 00:17:36 +0900

    Fixed a bug for 'buffer-move' in which Treemacs and NeTree buffers are treated as 'other-window'.

--- buffer-move.el
+++ buffer-move.el
@@ -71,11 +71,11 @@
   "Swap the current buffer and the buffer above the split.
 If there is no split, ie now window above the current one, an
 error is signaled."
-;;  "Switches between the current buffer, and the buffer above the
-;;  split, if possible."
+  ;; "Switches between the current buffer, and the buffer above the
+  ;; split, if possible."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'up))
-	 (buf-this-buf (window-buffer (selected-window))))
+	     (buf-this-buf (window-buffer (selected-window))))
     (if (null other-win)
         (error "No window above this one")
       ;; swap top with this one
@@ -86,12 +86,12 @@ error is signaled."
 
 ;;;###autoload
 (defun buf-move-down ()
-"Swap the current buffer and the buffer under the split.
+  "Swap the current buffer and the buffer under the split.
 If there is no split, ie now window under the current one, an
 error is signaled."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'down))
-	 (buf-this-buf (window-buffer (selected-window))))
+	     (buf-this-buf (window-buffer (selected-window))))
     (if (or (null other-win) 
             (string-match "^ \\*Minibuf" (buffer-name (window-buffer other-win))))
         (error "No window under this one")
@@ -103,13 +103,17 @@ error is signaled."
 
 ;;;###autoload
 (defun buf-move-left ()
-"Swap the current buffer and the buffer on the left of the split.
+  "Swap the current buffer and the buffer on the left of the split.
 If there is no split, ie now window on the left of the current
 one, an error is signaled."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'left))
-	 (buf-this-buf (window-buffer (selected-window))))
-    (if (null other-win)
+	     (buf-this-buf (window-buffer (selected-window))))
+    (if (or (null other-win)
+            (string-match "^ \\*Treemacs" (buffer-name (window-buffer other-win)))
+            (string-match "^ \\*Treemacs" (buffer-name buf-this-buf))
+            (string-match "^ \\*NeoTree" (buffer-name (window-buffer other-win)))
+            (string-match "^ \\*NeoTree" (buffer-name buf-this-buf)))
         (error "No left split")
       ;; swap top with this one
       (set-window-buffer (selected-window) (window-buffer other-win))
@@ -119,13 +123,17 @@ one, an error is signaled."
 
 ;;;###autoload
 (defun buf-move-right ()
-"Swap the current buffer and the buffer on the right of the split.
+  "Swap the current buffer and the buffer on the right of the split.
 If there is no split, ie now window on the right of the current
 one, an error is signaled."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'right))
-	 (buf-this-buf (window-buffer (selected-window))))
-    (if (null other-win)
+	     (buf-this-buf (window-buffer (selected-window))))
+    (if (or (null other-win)
+            (string-match "^ \\*Treemacs" (buffer-name (window-buffer other-win)))
+            (string-match "^ \\*Treemacs" (buffer-name buf-this-buf))
+            (string-match "^ \\*NeoTree" (buffer-name (window-buffer other-win)))
+            (string-match "^ \\*NeoTree" (buffer-name buf-this-buf)))
         (error "No right split")
       ;; swap top with this one
       (set-window-buffer (selected-window) (window-buffer other-win))
