086cee428eddcd157e368a188937edd56b200290
Richard Jaeho Hur <daftcoder@gmail.com>
2021-08-04 16:26:20 +0900

    Clean up the neotree configuration.

--- init.el
+++ init.el
@@ -206,9 +206,6 @@
   (load-theme 'doom-dracula t)
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  (setq doom-themes-neotree-file-icons t)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
@@ -917,6 +914,8 @@
 
 ;;==============================================================================
 ;; treemacs
+;;
+;; https://github.com/Alexander-Miller/treemacs
 ;;==============================================================================
 
 (use-package treemacs
@@ -926,55 +925,54 @@
   (with-eval-after-load 'winum
     (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (progn
-    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-extension-regex          treemacs-last-period-regex-value
-          treemacs-file-follow-delay             0.2
-          treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   2
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-missing-project-action        'ask
-          treemacs-no-png-images                 nil
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        nil
-          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                      'left
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    nil
-          treemacs-recenter-after-tag-follow     nil
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   nil
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-asc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              1.5
-          treemacs-width                         40)
+  (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+        treemacs-deferred-git-apply-delay      0.5
+        treemacs-display-in-side-window        t
+        treemacs-eldoc-display                 t
+        treemacs-file-event-delay              5000
+        treemacs-file-extension-regex          treemacs-last-period-regex-value
+        treemacs-file-follow-delay             0.2
+        treemacs-follow-after-init             t
+        treemacs-git-command-pipe              ""
+        treemacs-goto-tag-strategy             'refetch-index
+        treemacs-indentation                   2
+        treemacs-indentation-string            " "
+        treemacs-is-never-other-window         nil
+        treemacs-max-git-entries               5000
+        treemacs-missing-project-action        'ask
+        treemacs-no-png-images                 nil
+        treemacs-no-delete-other-windows       t
+        treemacs-project-follow-cleanup        nil
+        treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+        treemacs-position                      'left
+        treemacs-recenter-distance             0.1
+        treemacs-recenter-after-file-follow    nil
+        treemacs-recenter-after-tag-follow     nil
+        treemacs-recenter-after-project-jump   'always
+        treemacs-recenter-after-project-expand 'on-distance
+        treemacs-show-cursor                   nil
+        treemacs-show-hidden-files             t
+        treemacs-silent-filewatch              nil
+        treemacs-silent-refresh                nil
+        treemacs-sorting                       'alphabetic-asc
+        treemacs-space-between-root-nodes      t
+        treemacs-tag-follow-cleanup            t
+        treemacs-tag-follow-delay              1.5
+        treemacs-width                         40)
 
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
+  ;; The default width and height of the icons is 22 pixels. If you are
+  ;; using a Hi-DPI display, uncomment this to double the icon size.
+  ;;(treemacs-resize-icons 44)
 
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+  (treemacs-fringe-indicator-mode t)
+  (pcase (cons (not (null (executable-find "git")))
+               (not (null treemacs-python-executable)))
+    (`(t . t)
+     (treemacs-git-mode 'deferred))
+    (`(t . _)
+     (treemacs-git-mode 'simple)))
 
   (defun treemacs-hide()
     "Close the Treemacs window."
@@ -1010,43 +1008,53 @@
 
 ;;==============================================================================
 ;; neotree
+;;
+;; https://github.com/jaypei/emacs-neotree
 ;;==============================================================================
 
 (use-package neotree
   :ensure t
   :config
-  (progn
-    (setq neo-window-width 40
-          neo-window-fixed-size t
-          neo-smart-open t
-          neo-vc-integration (quote (face)) ;; https://github.com/jaypei/emacs-neotree/issues/166
-          projectile-switch-project-action 'neotree-projectile-action) ;; To open neotree when projectile project is opend.
+  (if (and (fboundp 'doom-themes-neotree-config)
+           nil) ;; Currently not used because doom-themes-neotree-config doesn't support neo-vc-integration.
+      (progn
+        ;; Enable custom neotree theme (all-the-icons must be installed!)
+        (doom-themes-neotree-config)
+        (setq doom-themes-neotree-file-icons t))
+    (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
-    (defun neotree-toggle-project-root-dir-or-current-dir ()
-      "Open NeoTree using the project root, using projectile, or the current buffer directory."
-      (interactive)
-      (let ((project-dir (ignore-errors (projectile-project-root)))
-            (file-name (buffer-file-name)))
-        (if (neo-global--window-exists-p)
-            (neotree-hide)
-	      (progn
-            (neotree-show)
-            (if project-dir
-                (neotree-dir project-dir))
-            (if file-name
-                (neotree-find file-name))))))
+  (setq neo-window-width 40
+        neo-window-fixed-size t
+        neo-show-hidden-files t
+        neo-smart-open t
+        neo-vc-integration (quote (face char)) ;; https://github.com/jaypei/emacs-neotree/issues/166
+        projectile-switch-project-action 'neotree-projectile-action) ;; To open neotree when projectile project is opend.
 
-    (defun neotree-show-project-root-dir ()
-      "Show NeoTree using the project root using projectile."
-      (interactive)
-      (let ((project-dir (projectile-project-root))
-            (file-name (buffer-file-name)))
-        (progn
+  (defun neotree-toggle-project-root-dir-or-current-dir ()
+    "Open NeoTree using the project root, using projectile, or the current buffer directory."
+    (interactive)
+    (let ((project-dir (ignore-errors (projectile-project-root)))
+          (file-name (buffer-file-name)))
+      (if (neo-global--window-exists-p)
+          (neotree-hide)
+	    (progn
           (neotree-show)
-	      (if project-dir
+          (if project-dir
               (neotree-dir project-dir))
-	      (if file-name
+          (if file-name
               (neotree-find file-name))))))
+
+  (defun neotree-show-project-root-dir ()
+    "Show NeoTree using the project root using projectile."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (progn
+        (neotree-show)
+	    (if project-dir
+            (neotree-dir project-dir))
+	    (if file-name
+            (neotree-find file-name)))))
   :bind
   (:map global-map
         ("C-x t n" . neotree-toggle-project-root-dir-or-current-dir)))
