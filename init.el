b44f367be0ca326a9ea55c739fb89f9d9e1070e0
Richard Jaeho Hur <daftcoder@gmail.com>
2022-01-16 16:27:31 +0900

    Added 'md4rd'.

--- init.el
+++ init.el
@@ -137,7 +137,7 @@
      ("\\.x?html?\\'" . default)
      ("\\.pdf\\'" . emacs)))
  '(package-selected-packages
-   '(string-utils flx exec-path-from-shell all-the-icons all-the-icons-ivy all-the-icons-ivy-rich doom-themes doom-modeline nyan-mode centaur-tabs page-break-lines dashboard which-key hydra pretty-hydra flycheck magit projectile restclient docker typescript-mode dockerfile-mode org-tree-slide command-log-mode perspective magic-latex-buffer px ein yaml-mode hide-mode-line use-package bind-key google-c-style i3wm-config-mode peep-dired swift-mode focus cuda-mode org-bullets org-re-reveal markdown-preview-mode graphviz-dot-mode ivy counsel counsel-projectile counsel-at-point swiper ivy-posframe ivy-rich diff-hl spell-fu treemacs treemacs-projectile treemacs-icons-dired treemacs-magit qml-mode keyfreq neato-graph-bar arxiv-mode epc importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump highlight-indent-guides highlight-indentation filldent lsp-mode lsp-ui lsp-treemacs lsp-ivy lsp-pyright dap-mode ccls pdf-tools helm-gtags helm-lsp imenu-list objc-font-lock neotree company company-fuzzy company-statistics company-box company-restclient vlf haskell-mode lua-mode ztree undo-tree shrink-path rich-minority pyvenv markdown-mode magit-popup helm find-file-in-project evil avy ace-window)))
+   '(string-utils flx exec-path-from-shell all-the-icons all-the-icons-ivy all-the-icons-ivy-rich doom-themes doom-modeline nyan-mode centaur-tabs page-break-lines dashboard which-key hydra pretty-hydra flycheck magit projectile restclient docker typescript-mode dockerfile-mode org-tree-slide command-log-mode perspective magic-latex-buffer px ein yaml-mode hide-mode-line use-package bind-key google-c-style i3wm-config-mode peep-dired swift-mode focus cuda-mode org-bullets org-re-reveal markdown-preview-mode graphviz-dot-mode ivy counsel counsel-projectile counsel-at-point swiper ivy-posframe ivy-rich diff-hl spell-fu treemacs treemacs-projectile treemacs-icons-dired treemacs-magit qml-mode keyfreq neato-graph-bar arxiv-mode md4rd epc importmagic pip-requirements py-autopep8 elpy json-reformat yasnippet rg deadgrep ripgrep helm-rg ag helm-ag dumb-jump highlight-indent-guides highlight-indentation filldent lsp-mode lsp-ui lsp-treemacs lsp-ivy lsp-pyright dap-mode ccls pdf-tools helm-gtags helm-lsp imenu-list objc-font-lock neotree company company-fuzzy company-statistics company-box company-restclient vlf haskell-mode lua-mode ztree undo-tree shrink-path rich-minority pyvenv markdown-mode magit-popup helm find-file-in-project evil avy ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -2440,6 +2440,23 @@ If optional arg SILENT is non-nil, do not display progress messages."
 
 (use-package arxiv-mode
   :ensure t)
+
+;;==============================================================================
+;; md4rd - Mode for reddit
+;;
+;; https://github.com/ahungry/md4rd
+;;==============================================================================
+
+(use-package md4rd
+  :ensure t
+  :config
+  (add-hook 'md4rd-mode-hook 'md4rd-indent-all-the-lines)
+  (setq md4rd-subs-active '(emacs lisp+Common_Lisp prolog clojure))
+  (setq md4rd--oauth-access-token "44428323-ci7Q1lW1XZadFiMIYPdQu2Xdj-asyw")
+  (setq md4rd--oauth-refresh-token "44428323-vwOa5w774LmcjoMcy7IxTySny2aXBw")
+  (run-with-timer 0 3540 'md4rd-refresh-login)
+  (message "md4rd--oauth-access-token: %s" md4rd--oauth-access-token)
+  (message "md4rd--oauth-refresh-token: %s" md4rd--oauth-refresh-token))
 
 ;;==============================================================================
 ;; Instant Stackoverflow Solutions
