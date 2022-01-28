7aa75e02f2322c4662e61ebe2d77496972fec7d6
Richard Jaeho Hur <daftcoder@gmail.com>
2022-01-28 17:58:02 +0900

    Upgraded 'stackoverflow' mode.

--- stackoverflow.el
+++ stackoverflow.el
@@ -39,49 +39,53 @@
                             (> (assoc-default 'score x)
                                (assoc-default 'score y)))))))
 
-    (switch-to-buffer "*stackexchange*")
-    (erase-buffer)
+    (let ((buffer-name (concat "*stackexchange* (" query ")")))
+      (let ((buffer (get-buffer buffer-name)))
+        (when buffer
+          (kill-buffer buffer)))
 
-    (dolist (question_id (mapcar 'string-to-number question_ids))
-      (let ((question (some (lambda (question)
-                              (if (equal (assoc-default 'question_id question)
-                                         question_id)
-                                  question))
-                            questions)))
-        (insert "<hr><h2 style='background-color:paleturquoise'>Question: "
-                (format "<a href='%s'>%s</a>"
-                        (assoc-default 'link question)
-                        (assoc-default 'title question))
-                "</h2>"
-                "\n"
-                (mapconcat
-                 'identity
-                 (let ((rendered
-                        (remove-if
-                         'null
-                         (mapcar (lambda (answer)
-                                   (if (and (equal question_id
-                                                   (assoc-default 'question_id answer))
-                                            (>= (assoc-default 'score answer) 0))
-                                       (concat "<hr><h2 style='background-color:"
-                                               "#c1ffc1'>Answer - score: "
-                                               (number-to-string (assoc-default 'score answer))
-                                               "</h2>"
-                                               (assoc-default 'body answer))))
-                                 answers))))
-                   (if (> (length rendered) 5)
-                       (append (subseq rendered 0 5)
-                               (list (format "<br><br><a href='%s'>%s</a>"
-                                             (assoc-default 'link question)
-                                             "More answers...")))
-                     rendered))
-                 "\n")
-                )))
-    (shr-render-region (point-min) (point-max))
-    (goto-char (point-min))
-    (save-excursion
-      (while (search-forward "^M" nil t)
-        (replace-match "")))))
+      (let ((buffer (get-buffer-create buffer-name)))
+        (switch-to-buffer buffer)
+        (dolist (question_id (mapcar 'string-to-number question_ids))
+          (let ((question (some (lambda (question)
+                                  (if (equal (assoc-default 'question_id question)
+                                             question_id)
+                                      question))
+                                questions)))
+            (insert "<hr><h1 style='background-color:#383c4d'>&nbsp;"
+                    (format "<a href='%s'>%s</a>"
+                            (assoc-default 'link question)
+                            (assoc-default 'title question))
+                    "</h1>"
+                    "\n"
+                    (mapconcat
+                     'identity
+                     (let ((rendered
+                            (remove-if
+                             'null
+                             (mapcar (lambda (answer)
+                                       (if (and (equal question_id
+                                                       (assoc-default 'question_id answer))
+                                                (>= (assoc-default 'score answer) 0))
+                                           (concat "<hr><h2 style='background-color:#2f3240'>&nbsp;Answer - Score: "
+                                                   (number-to-string (assoc-default 'score answer))
+                                                   "</h2>"
+                                                   (assoc-default 'body answer))))
+                                     answers))))
+                       (if (> (length rendered) 5)
+                           (append (subseq rendered 0 5)
+                                   (list (format "<br><br><a href='%s'>%s</a>"
+                                                 (assoc-default 'link question)
+                                                 "More answers...")))
+                         rendered))
+                     "\n")
+                    )))
+        (shr-render-region (point-min) (point-max))
+        (goto-char (point-min))
+        (save-excursion
+          (while (search-forward "^M" nil t)
+            (replace-match "")))
+        (read-only-mode)))))
 
 (defun stackoverflow-lookup ()
   (interactive)
