0806442cbaca520cb5111c1e6af684a41454e0b7
Richard Jaeho Hur <daftcoder@gmail.com>
2022-07-12 09:36:54 +0900

    'large-file-warning-threshold' modified.

--- init-base.el
+++ init-base.el
@@ -20,7 +20,7 @@
       split-height-threshold nil ; not to split this way.
       register-preview-delay 0
       gdb-many-windows t
-      large-file-warning-threshold 50000000)
+      large-file-warning-threshold (* 100 1024 1024))
 (blink-cursor-mode 0)
 (setq-default truncate-lines t)
 (setq-default tab-width 4)
