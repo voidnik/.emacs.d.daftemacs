3b16dc62ca25879fc5542183a4a7ef93b2930b0e
Richard Jaeho Hur <daftcoder@gmail.com>
2022-11-16 00:19:12 +0900

    Override the key 'M-]' to 'centaur-tabs-forward' in 'vterm-mode'.

--- init.el
+++ init.el
@@ -2353,6 +2353,7 @@ If optional arg SILENT is non-nil, do not display progress messages."
 (use-package multi-vterm
   :config
   (add-hook 'vterm-mode-hook (lambda ()
+                               (define-key vterm-mode-map (kbd "M-]") 'centaur-tabs-forward)
                                (define-key vterm-mode-map (kbd "C-c r") 'multi-vterm-rename-buffer))))
 
 ;;==============================================================================
