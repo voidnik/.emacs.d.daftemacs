c4d7fd681826a52d14f292655bf0510bc43220d9
Richard Jaeho Hur <daftcoder@gmail.com>
2023-02-02 09:51:17 +0900

    Modified the installation script for ccls.

--- install-ccls.sh
+++ install-ccls.sh
@@ -4,7 +4,8 @@ git clone --depth=1 --recursive https://github.com/MaskRay/ccls
 cd ccls
 
 if [ "$(uname)" == "Darwin" ]; then
-    cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/local/opt/llvm
+    brew install llvm
+    cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$(brew --prefix llvm)/lib/cmake"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/lib/llvm-7
 else
