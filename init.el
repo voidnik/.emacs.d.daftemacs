2d44f2a7fcf516a1d90c380a459131702aaeec3d
Richard Jaeho Hur <daftcoder@gmail.com>
2026-06-23 10:41:27 +0900

    Modify lua-indent-level setting to 2.

--- init.el
+++ init.el
@@ -3294,7 +3294,8 @@ If optional arg SILENT is non-nil, do not display progress messages."
   :config
   (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
   (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-  (add-to-list 'interpreter-mode-alist '("lua" . lua-mode)))
+  (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+  (setq lua-indent-level 2))
 
 ;;==============================================================================
 ;; Objective C
