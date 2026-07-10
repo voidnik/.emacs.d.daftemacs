fe83a06ed37f072e828294b5a9dccc416fde6c51
Richard Jaeho Hur <daftcoder@gmail.com>
2026-07-10 15:04:41 +0900

    Add a temporary solution for treemacs
    When trying to change the file name by pressing R  on a file in Emacs' treemacs, the message "face-spec-set-2: Face inheritance results in inheritance cycle: gnus-group-news-low" appears and does not work properly.

--- init.el
+++ init.el
@@ -787,7 +787,14 @@ even when the file is larger than `large-file-warning-threshold'.")
   ;; or for treemacs users
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (doom-themes-org-config)
+
+  ;; Temporary solution: When trying to change the file name by pressing R
+  ;; on a file in Emacs' treemacs, the message "face-spec-set-2: Face
+  ;; inheritance results in inheritance cycle: gnus-group-news-low" appears
+  ;; and does not work properly.
+  (setcdr (assoc 'gnus-group-news-low-empty doom-themes-base-faces)
+          '(:inherit 'gnus-group-mail-1-empty :weight 'normal)))
 
 ;; https://draculatheme.com/emacs/
 ;;(load-file "~/.emacs.d/dracula-theme.el")
